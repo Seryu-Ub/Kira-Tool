aafc6beead239c796b165ab4c2d0aeb89c379a96
Seryû Ubiquitous <tatsumi.loulou@gmail.com>
2021-02-19 06:08:54 +0100

    🐧

--- Kira/Program.cs
+++ Kira/Program.cs
@@ -33,7 +33,6 @@ namespace Kira_Tool
         private static Task _client_Log(LogMessage arg)
         {
             Console.WriteLine(Environment.NewLine + arg, System.Drawing.Color.DarkCyan);
-            Console.Clear();
             return Task.CompletedTask;
         }
 
@@ -44,7 +43,7 @@ namespace Kira_Tool
             Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
             Console.WriteLine("Connecté sur l'utilisateur :" + _client.CurrentUser.Username + "#" + _client.CurrentUser.Discriminator, System.Drawing.Color.MediumPurple);
             Console.WriteLine("--------------------------------------" + Environment.NewLine, System.Drawing.Color.MediumPurple);
-            Console.WriteLine("[1] Twitch Presence");
+            Console.WriteLine("[1] Twitch Presence   #####   [2] Playing Presence   ####   [3] Watching Presence   ####   [4] Listening Presence" + Environment.NewLine, System.Drawing.Color.HotPink);
 
             string up = Console.ReadLine();
 
@@ -56,6 +55,7 @@ namespace Kira_Tool
                 Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
                 Console.WriteLine("Connecté sur l'utilisateur :" + _client.CurrentUser.Username + "#" + _client.CurrentUser.Discriminator, System.Drawing.Color.MediumPurple);
                 Console.WriteLine("--------------------------------------" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+                Console.WriteLine(Environment.NewLine + "Si vous voulez supprimer votre présence il suffit de ne rien écrire et de presser \"Entrée\"" + Environment.NewLine, System.Drawing.Color.Cyan);
                 Console.WriteLine("Veuillez saisir le message de votre présence :" + Environment.NewLine);
                 string rpc = Console.ReadLine();
                 Console.WriteLine(Environment.NewLine + "Veuillez saisir votre lien twitch :" + Environment.NewLine);
@@ -63,10 +63,76 @@ namespace Kira_Tool
 
                 await _client.SetGameAsync(rpc, link, ActivityType.Streaming);
 
+                Console.WriteLine(Environment.NewLine + "Votre présence a été changé avec succès !" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+
+                await Task.Delay(4000);
+
                 await Program.ReadyApp();
-
-
             }
+
+            if (up == "2")
+            {
+                Console.Clear();
+                Console.Title = "Discord Kira-Tool | Playing Presence";
+                Console.WriteAscii("Discord Kira-Tool V.1", System.Drawing.Color.DeepPink);
+                Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
+                Console.WriteLine("Connecté sur l'utilisateur :" + _client.CurrentUser.Username + "#" + _client.CurrentUser.Discriminator, System.Drawing.Color.MediumPurple);
+                Console.WriteLine("--------------------------------------" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+                Console.WriteLine(Environment.NewLine + "Si vous voulez supprimer votre présence il suffit de ne rien écrire et de presser \"Entrée\"" + Environment.NewLine, System.Drawing.Color.Cyan);
+                Console.WriteLine("Veuillez saisir le message de votre présence :" + Environment.NewLine);
+                string rpc = Console.ReadLine();
+
+                await _client.SetActivityAsync(new Game(rpc, ActivityType.Playing));
+
+                Console.WriteLine(Environment.NewLine + "Votre présence a été changé avec succès !" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+
+                await Task.Delay(4000);
+
+                await Program.ReadyApp();
+            }
+
+            if (up == "3")
+            {
+                Console.Clear();
+                Console.Title = "Discord Kira-Tool | Watching Presence";
+                Console.WriteAscii("Discord Kira-Tool V.1", System.Drawing.Color.DeepPink);
+                Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
+                Console.WriteLine("Connecté sur l'utilisateur :" + _client.CurrentUser.Username + "#" + _client.CurrentUser.Discriminator, System.Drawing.Color.MediumPurple);
+                Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
+                Console.WriteLine(Environment.NewLine + "Si vous voulez supprimer votre présence il suffit de ne rien écrire et de presser \"Entrée\"" + Environment.NewLine, System.Drawing.Color.Cyan);
+                Console.WriteLine("Veuillez saisir le message de votre présence :" + Environment.NewLine);
+                string rpc = Console.ReadLine();
+
+                await _client.SetActivityAsync(new Game(rpc, ActivityType.Watching));
+
+                Console.WriteLine(Environment.NewLine + "Votre présence a été changé avec succès !" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+
+                await Task.Delay(4000);
+
+                await Program.ReadyApp();
+            }
+
+            if (up == "4")
+            {
+                Console.Clear();
+                Console.Title = "Discord Kira-Tool | Listening Presence";
+                Console.WriteAscii("Discord Kira-Tool V.1", System.Drawing.Color.DeepPink);
+                Console.WriteLine("--------------------------------------", System.Drawing.Color.MediumPurple);
+                Console.WriteLine("Connecté sur l'utilisateur :" + _client.CurrentUser.Username + "#" + _client.CurrentUser.Discriminator, System.Drawing.Color.MediumPurple);
+                Console.WriteLine("--------------------------------------" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+                Console.WriteLine(Environment.NewLine + "Si vous voulez supprimer votre présence il suffit de ne rien écrire et de presser \"Entrée\"" + Environment.NewLine, System.Drawing.Color.Cyan);
+                Console.WriteLine("Veuillez saisir le message de votre présence :" + Environment.NewLine);
+                string rpc = Console.ReadLine();
+
+                await _client.SetActivityAsync(new Game(rpc, ActivityType.Listening));
+
+                Console.WriteLine(Environment.NewLine + "Votre présence a été changé avec succès !" + Environment.NewLine, System.Drawing.Color.MediumPurple);
+
+                await Task.Delay(4000);
+
+                await Program.ReadyApp();
+            }
+
             return Task.CompletedTask;
         }
     }
